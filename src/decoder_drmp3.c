51ee4b3c6bed1d2aa61b193cdfc4d924c143391b
Ryan C. Gordon <icculus@icculus.org>
2025-04-06 22:27:18 -0400

    dr_mp3: Fixed seek table generation.

--- src/decoder_drmp3.c
+++ src/decoder_drmp3.c
@@ -86,7 +86,6 @@ static bool SDLCALL DRMP3_init_audio(SDL_IOStream *io, SDL_AudioSpec *spec, SDL_
     drmp3_uint64 num_mp3_frames = 0;
     drmp3_uint64 num_pcm_frames = 0;
     if (drmp3_get_mp3_and_pcm_frame_count(&decoder, &num_mp3_frames, &num_pcm_frames)) {
-        #if 0  // !!! FIXME: seek points are causing incorrect seeks, so it's either buggy or I'm doing this wrong.
         payload->num_seek_points = (drmp3_uint32) num_mp3_frames;
         payload->seek_points = (drmp3_seek_point *) SDL_calloc(num_mp3_frames, sizeof (*payload->seek_points));
         if (payload->seek_points) {
@@ -104,7 +103,6 @@ static bool SDLCALL DRMP3_init_audio(SDL_IOStream *io, SDL_AudioSpec *spec, SDL_
                 payload->num_seek_points = 0;
             }
         }
-        #endif
     }
 
     spec->format = SDL_AUDIO_F32;
