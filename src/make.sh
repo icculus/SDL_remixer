3f56b0dbdbb479b437f6bc9ed03ee40bd0374416
Ryan C. Gordon <icculus@icculus.org>
2025-04-14 10:59:38 -0400

    Moved off make.sh into a "proper" CMake project.
    This is _also_ meant to be thrown away, but we're at the point where it's a
    benefit to do parallel builds and only compile what changed.
    
    This still expects to use pkg-config, so it's not going to work on Windows
    or Mac, etc.
    
    Reference Issue #2.

--- src/make.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-gcc -O0 -Wall -ggdb3 -I../include -DVORBIS_DYNAMIC='"libvorbisfile.so.3"' -DOPUS_DYNAMIC='"libopusfile.so.0"' -DWAVPACK_DYNAMIC='"libwavpack.so.1"' -o testmixer SDL_mixer.c SDL_mixer_metadata_tags.c decoder_wav.c decoder_raw.c decoder_aiff.c decoder_voc.c decoder_sinewave.c decoder_drmp3.c decoder_vorbis.c decoder_opus.c decoder_wavpack.c decoder_drflac.c testmixer.c `pkg-config sdl3 --cflags --libs` `pkg-config opus --cflags`
-
