537c14a0c32eadf260e0a036436c9b88c8f20b54
Ryan C. Gordon <icculus@icculus.org>
2025-04-25 22:11:32 -0400

    opus: Fixed copy/pasted comment.

--- src/decoder_opus.c
+++ src/decoder_opus.c
@@ -19,7 +19,7 @@
   3. This notice may not be removed or altered from any source distribution.
 */
 
-// This file supports Ogg Vorbis audio streams using libvorbisfile (or the integer-only implementation, "tremor").
+// This file supports Ogg Opus audio streams using libopusfile.
 
 #include "SDL_mixer_internal.h"
 
